ae491210cc24411eee716661fe3a6f37020443a1
Djordje Mancović <dj.mancovic@gmail.com>
2015-02-08 11:09:51 +0100

    order email change

--- wp-content/plugins/woocommerce/templates/emails/customer-processing-order.php
+++ wp-content/plugins/woocommerce/templates/emails/customer-processing-order.php
@@ -30,12 +30,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 		<?php
 			if ( $totals = $order->get_order_item_totals() ) {
 				$i = 0;
-				foreach ( $totals as $total ) {
-					$i++;
-					?><tr>
-						<th scope="row" colspan="2" style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['label']; ?></th>
-					</tr><?php
-				}
 			}
 		?>
 	</tfoot>
