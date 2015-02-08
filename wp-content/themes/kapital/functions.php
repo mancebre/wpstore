18aa0c9ebac390d9fc3bf1c04a633d65d73f37e4
Djordje Mancović <dj.mancovic@gmail.com>
2015-02-08 18:39:49 +0100

    remove prices

--- wp-content/themes/kapital/functions.php
+++ wp-content/themes/kapital/functions.php
@@ -638,8 +638,9 @@
     add_action('admin_enqueue_scripts','admin_scripts_styles');
 
     /* REMOVE PRICES */
+    // remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+    // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
     remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
     /* END REMOVE PRICES */
 
