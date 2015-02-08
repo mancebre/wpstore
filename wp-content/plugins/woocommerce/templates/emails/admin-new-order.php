64290f8662a4f4122becb1daf888e3bbbed4c638
Djordje Mancović <dj.mancovic@gmail.com>
2015-02-08 22:45:36 +0100

    remove totals

--- wp-content/plugins/woocommerce/templates/emails/admin-new-order.php
+++ wp-content/plugins/woocommerce/templates/emails/admin-new-order.php
@@ -26,18 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 		<?php echo $order->email_order_items_table( false, true ); ?>
 	</tbody>
 	<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) {
-				$i = 0;
-				foreach ( $totals as $total ) {
-					$i++;
-					?><tr>
-						<th scope="row" colspan="2" style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['label']; ?></th>
-						<td style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['value']; ?></td>
-					</tr><?php
-				}
-			}
-		?>
 	</tfoot>
 </table>
 
